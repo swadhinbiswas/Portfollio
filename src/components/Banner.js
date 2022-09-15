cbd274e6c2aa5ce75f1efb15f301953ab12feb86
Swadhin Biswas <107450069+swadhinbiswas@users.noreply.github.com>
2022-09-16 03:56:17 +0600

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm SWADHIN HERE AS `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>And a person passionate with Data science .Now i am persunig Bachelor Degree on Computer science from Daffodil International University ,Dhaka,Bangladesh..</p>
+                  <p>And a person passionate with Data science .Now i am parsuing Bachelor Degree on Computer science from Daffodil International University ,Dhaka,Bangladesh..</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
